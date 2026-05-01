cd59976513f06a5051a557bd9c91c8ddf121feaa
Henadzi Shutko <hazardsoft@gmail.com>
2026-05-01 18:02:25 +0300

    feat: save search input in local storage

--- src/features/search/search.tsx
+++ src/features/search/search.tsx
@@ -1,12 +1,57 @@
-import { Component, type ReactNode } from 'react';
+import { Component, createRef, type ChangeEvent, type ReactNode } from 'react';
 import './search.css';
 
 export default class Search extends Component {
+  inputRef = createRef<HTMLInputElement>();
+
+  state = {
+    searchItem: '',
+  };
+
+  readFromLocalStorage = (): string => {
+    try {
+      return localStorage.getItem('searchItem') ?? '';
+    } catch {
+      return '';
+    }
+  };
+
+  writeToLocalStorage = (value: string): void => {
+    if (!value) return;
+    try {
+      localStorage.setItem('searchItem', value);
+    } catch {
+      /* empty */
+    }
+  };
+
+  onSearch = (): void => {
+    const searchItem = (this.inputRef.current?.value ?? '').trim();
+    this.writeToLocalStorage(searchItem);
+    this.setState({ searchItem });
+    this.inputRef.current?.focus();
+  };
+
+  onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const searchItem = event.target.value.trim();
+    this.setState({ searchItem });
+  };
+
+  componentDidMount(): void {
+    this.setState({
+      searchItem: this.readFromLocalStorage(),
+    });
+  }
+
   render(): ReactNode {
     return (
       <section id="search" className="search">
-        <input type=""></input>
-        <button>Search</button>
+        <input
+          ref={this.inputRef}
+          value={this.state.searchItem}
+          onChange={this.onChange}
+        ></input>
+        <button onClick={this.onSearch}>Search</button>
       </section>
     );
   }
