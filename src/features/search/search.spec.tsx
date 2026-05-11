d98cf65425b043e117c75da38f4f522b722e4a7a
Henadzi Shutko <henadzi.shutko@broit.io>
2026-05-11 22:21:16 +0300

    refactor: remove localStorage mock

--- src/features/search/search.spec.tsx
+++ src/features/search/search.spec.tsx
@@ -1,18 +1,10 @@
 import { render, screen } from '@testing-library/react';
-import { describe, expect, it, vi } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import Search from './search';
-import { readSearchItem } from '../../api/local';
-
-vi.mock('../../api/local', () => {
-  return {
-    readSearchItem: vi.fn(),
-    writeSearchItem: vi.fn(),
-  };
-});
 
 describe('Search', () => {
   it('Search value is empty if local storage returns nothing', async () => {
-    vi.mocked(readSearchItem).mockReturnValue('');
+    localStorage.setItem('searchItem', '');
 
     render(<Search handleSearch={() => {}} />);
     expect(screen.getByRole('searchbox')).toHaveValue('');
@@ -20,7 +12,7 @@ describe('Search', () => {
 
   it('Search value is equal to local storage value is it retuens smth', async () => {
     const searchItem = 'bulbasaur';
-    vi.mocked(readSearchItem).mockReturnValue(searchItem);
+    localStorage.setItem('searchItem', searchItem);
 
     render(<Search handleSearch={() => {}} />);
     expect(screen.getByRole('searchbox')).toHaveValue(searchItem);
