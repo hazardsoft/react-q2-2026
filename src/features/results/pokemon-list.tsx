0bdc65a8b5c5ab5e5170170d5d46c47b3665611c
Henadzi Shutko <henadzi.shutko@broit.io>
2026-05-16 22:03:45 +0300

    refactor(list): convert PokemonList from class component to functional

--- src/features/results/pokemon-list.tsx
+++ src/features/results/pokemon-list.tsx
@@ -1,4 +1,3 @@
-import { Component, type ReactNode } from 'react';
 import './pokemon-list.css';
 import type { Pokemon } from '../../api/types';
 import PokermonCard from './pokermon-card';
@@ -7,17 +6,17 @@ type PokemonListProps = {
   pokemons: Pokemon[];
 };
 
-export default class PokemonList extends Component<PokemonListProps> {
-  render(): ReactNode {
-    if (this.props.pokemons.length === 0) {
-      return <p>No Pokemons</p>;
-    }
-    return (
-      <div className="pokemon-list">
-        {this.props.pokemons.map((p) => (
-          <PokermonCard name={p.name} key={p.name} />
-        ))}
-      </div>
-    );
+const PokemonList = ({ pokemons }: PokemonListProps) => {
+  if (pokemons.length === 0) {
+    return <p>No Pokemons</p>;
   }
-}
+  return (
+    <div className="pokemon-list">
+      {pokemons.map((p) => (
+        <PokermonCard name={p.name} key={p.name} />
+      ))}
+    </div>
+  );
+};
+
+export default PokemonList;
